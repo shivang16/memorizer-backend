927c8cac3646e8d7fe613d4a490e8dd01c235da9
Shivang Mangal <shivangmangal@gmail.com>
2022-07-04 12:36:17 +0530

    Initial commit, build successful

---
+++ src/main/java/com/example/memorizerbackend/MemorizerBackendApplication.java
@@ -0,0 +1,24 @@
+package com.example.memorizerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
+
+@RestController
+public class MemorizerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorizerBackendApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/com/example/memorizerbackend/MemorizerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memorizerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorizerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
